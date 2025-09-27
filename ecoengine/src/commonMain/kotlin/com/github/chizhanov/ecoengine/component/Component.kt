dd7dae6d971a89a9d6d9b9cdcb0bbfe9b46b6f4d
chizhanov <5873123+chizhanov@users.noreply.github.com>
2025-09-27 21:47:37 +0300

    Add Z index.

--- ecoengine/src/commonMain/kotlin/com/github/chizhanov/ecoengine/component/Component.kt
+++ ecoengine/src/commonMain/kotlin/com/github/chizhanov/ecoengine/component/Component.kt
@@ -1,20 +1,37 @@
 package com.github.chizhanov.ecoengine.component
 
 import androidx.compose.ui.graphics.Canvas
+import kotlin.properties.Delegates
 
 open class Component {
     val children = mutableListOf<Component>()
     var isVisible: Boolean = true
+    var parent: Component? = null
+        private set
+
+    var zIndex: Int by Delegates.observable(0) { _, _, _ ->
+        parent?.sortChildren()
+    }
+
+    private fun sortChildren() {
+        children.sortBy { it.zIndex }
+    }
 
     fun add(child: Component) {
-        children.add(child)
+        child.parent = this
+        val insertionPoint = children.binarySearchBy(child.zIndex) { it.zIndex }
+        val index = if (insertionPoint < 0) -insertionPoint - 1 else insertionPoint
+        children.add(index, child)
     }
 
     fun addAll(children: List<Component>) {
+        children.forEach { it.parent = this }
         this.children.addAll(children)
+        sortChildren()
     }
 
     fun remove(child: Component) {
+        child.parent = null
         children.remove(child)
     }
 
@@ -32,6 +49,7 @@ open class Component {
     open fun renderTree(canvas: Canvas) {
         if (!isVisible) return
         render(canvas)
+        // Так как список children всегда отсортирован, отрисовка происходит в правильном порядке.
         for (child in children) {
             child.renderTree(canvas)
         }
